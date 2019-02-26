6af356d4a2de3d29dd07fd9889a75e690b60c84a
alexleru <alexleru@yandex.ru>
2019-02-26 21:39:00 +0300

    lesson1

--- app/src/main/java/ru/alexey/wheather/MainActivity.java
+++ app/src/main/java/ru/alexey/wheather/MainActivity.java
@@ -2,12 +2,18 @@ package ru.alexey.wheather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.wheather);
+        textView.setText(getString(R.string.label));
     }
 }
