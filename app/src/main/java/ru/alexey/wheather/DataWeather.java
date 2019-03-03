b382a8148a3887c84854b743663490eb25b8473a
alexleru <alexleru@yandex.ru>
2019-03-03 22:07:14 +0300

    lesson2

---
+++ app/src/main/java/ru/alexey/wheather/DataWeather.java
@@ -0,0 +1,10 @@
+package ru.alexey.wheather;
+
+
+//Создал временно, типа поставшик данных
+class DataWeather {
+    static String getData(){
+        return "30 °C. Ветер северный, 3-4 м/с";
+    }
+
+}
--- app/src/main/java/ru/alexey/wheather/MainActivity.java
+++ app/src/main/java/ru/alexey/wheather/MainActivity.java
@@ -2,18 +2,27 @@ package ru.alexey.wheather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView;
+    private TextView textView;
+    private TextView textCell;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        textView = findViewById(R.id.wheather);
+        //Задание к уроку 1. Просто вывести текст
+        textView = findViewById(R.id.weather);
         textView.setText(getString(R.string.label));
+
+        //Задание к уроку 2.
+        // В activity погодного приложения создайте пользовательский интерфейс
+        // в виде вывода на экран погодных значений.
+        textCell = findViewById(R.id.linear_layout_cell);
+        textCell.setText(DataWeather.getData());
+
     }
 }
