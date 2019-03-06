16e5a2c0dfa794d2d29c9704c42e9f9f44f4c68a
alexleru <alexleru@yandex.ru>
2019-03-06 23:59:49 +0300

    lesson3

--- app/src/main/java/ru/alexey/wheather/DataWeather.java
+++ app/src/main/java/ru/alexey/wheather/DataWeather.java
@@ -3,8 +3,24 @@ package ru.alexey.wheather;
 
 //Создал временно, типа поставшик данных
 class DataWeather {
-    static String getData(){
-        return "30 °C. Ветер северный, 3-4 м/с";
+
+    public static String getTemperature() {
+        return "30 °С";
     }
 
+    public static String getWind() {
+        return "3 м/с";
+    }
+
+    public static String getWindDirection() {
+        return "Северо-западный";
+    }
+
+    public static String getHumidity() {
+        return  "51%";
+    }
+
+    public static String getPressure() {
+        return "750 мм рт. ст.";
+    }
 }
--- app/src/main/java/ru/alexey/wheather/MainActivity.java
+++ app/src/main/java/ru/alexey/wheather/MainActivity.java
@@ -1,14 +1,22 @@
 package ru.alexey.wheather;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SwitchCompat;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
-    private TextView textCell;
+    private EditText editText;
+    private SwitchCompat [] switchCompats = new SwitchCompat[3];
+    final static String CITY = "CITY";
+    final static String ADDDATA = "ADDDATA";
+    private boolean [] showAddData = new boolean[3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +25,24 @@ public class MainActivity extends AppCompatActivity {
         //Задание к уроку 1. Просто вывести текст
         textView = findViewById(R.id.weather);
         textView.setText(getString(R.string.label));
+        editText = findViewById(R.id.edit_text);
+        switchCompats[0] = findViewById(R.id.show_wind);
+        switchCompats[1] = findViewById(R.id.show_humidity);
+        switchCompats[2] = findViewById(R.id.show_pressure);
+    }
 
-        //Задание к уроку 2.
-        // В activity погодного приложения создайте пользовательский интерфейс
-        // в виде вывода на экран погодных значений.
-        textCell = findViewById(R.id.linear_layout_cell);
-        textCell.setText(DataWeather.getData());
 
+    public void onClickBtnSearch(View view) {
+        Intent intent = new Intent(this, Main2Activity.class);
+        intent.putExtra(CITY, editText.getText().toString());
+        getDataFromswitchCompats();
+        intent.putExtra(ADDDATA, showAddData);
+        this.startActivity(intent);
+    }
+
+    public void getDataFromswitchCompats(){
+        for (int i = 0; i < showAddData.length; i++){
+                showAddData[i] = switchCompats[i].isChecked();
+        }
     }
 }
